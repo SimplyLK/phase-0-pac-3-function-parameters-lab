f2c96dc99f4234288800a6544b1ddd5cbe61ee78
Laura <laura.kan@hotmail.com>
2021-03-22 22:53:16 +1000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name = "Josh") {
+ return(`Hi, my name is ${name}.`);
+}
+introduction();
+
+function introductionWithLanguage (name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+introductionWithLanguage('Josh', 'Javascript');
+
+function introductionWithLanguageOptional(name = "User", language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+introductionWithLanguageOptional();
